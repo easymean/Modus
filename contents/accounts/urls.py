6190a14e89b07860e9694e2d2ee6b239c336352f
easymean <m2162003@gmail.com>
2020-07-31 10:11:34 +0900

    <refactor> change name of file

--- contents/accounts/urls.py
+++ contents/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', UserListView.as_view(), name='getUserList'),
-    path('signup', UserView.as_view(), name='signup'),
+    path('sign-up', UserView.as_view(), name='signUp'),
     path('<int:id>', UserView.as_view(), name='getUser'),
     path('<int:id>/password', UserView.as_view(), name='changePassword'),
     path('<int:id>/info', UserView.as_view(), name='updateInfo'),
